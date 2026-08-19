4d7a09d97651507c8deb4e2fe8c806edcef7f490
zougar99 <zougar99@users.noreply.github.com>
2026-08-19 23:07:08 +0100

    fix: Update profiledef.sh boot modes for latest archiso

--- iso/profile/profiledef.sh
+++ iso/profile/profiledef.sh
@@ -3,10 +3,10 @@
 
 iso_name="DevSecArch"
 iso_label="DEVSECARCH_2026"
-iso_publisher="DevSecArch <https://github.com/zougar99/DevSecArch-ISO>"
+iso_publisher="DevSecArch <https://github.com/zougar99/DevSecArch-systema>"
 iso_application="DevSecArch Linux"
 install_dir="arch"
-bootmodes=("bios.syslinux/legacy" "uefi-x64.systemd-boot")
+bootmodes=("bios.syslinux" "uefi-x64.systemd-boot")
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
